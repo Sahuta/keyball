1cb4173a1015cd8419ba55215f0413d9d17783e0
Sahuta <ruriboshi6464@gmail.com>
2024-08-03 13:52:44 +0900

    delete precise

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/custom/keymap.c
@@ -20,8 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include "quantum.h"
 
-// #include "precise.c"
-
 enum custom_keycodes {
     EISUU_LINUX = SAFE_RANGE,
     KANA_LINUX,
